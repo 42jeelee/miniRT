b02c2884de78cf8ce773421aec5f679df0992366
JEEHOON LEE <hoon@JEEHOONui-MacBookPro.local>
2023-06-07 00:43:25 +0900

    no change void type but ft_memset data

--- src/parse/parse_file.c
+++ src/parse/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 22:58:14 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/06 23:02:06 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/07 00:42:15 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int	parse_file(int ac, char **av, t_data *data)
 {
 	if (vaild_file(ac, av))
 		return (1);
-	(void)data;
+	ft_memset(data, 0, sizeof(t_data));
 	return (0);
 }
