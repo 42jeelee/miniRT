0fb28b49cd3d83ac7126b9255f846b3455ec39e1
Jeehoon Lee <jeelee@c10r2s4.42seoul.kr>
2023-06-06 22:28:39 +0900

    fixed ANSI escape sequence.

--- src/include/parse.h
+++ src/include/parse.h
@@ -6,23 +6,23 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:14:41 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/06 21:26:09 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/06 22:25:08 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-# define ANSI_RES "\e[0;0m"
-# define ANSI_BOL "\e[0;1m"
-# define ANSI_ITA "\e[0;3m"
-# define ANSI_BLC "\e[0;30m"
-# define ANSI_RED "\e[0;31m"
-# define ANSI_GRE "\e[0;32m"
-# define ANSI_YEL "\e[0;33m"
-# define ANSI_BLU "\e[0;34m"
-# define ANSI_MAG "\e[0;35m"
-# define ANSI_CYA "\e[0;36m"
+# define ANSI_RES "\e[0m"
+# define ANSI_BOL "\e[1m"
+# define ANSI_ITA "\e[3m"
+# define ANSI_BLC "\e[30m"
+# define ANSI_RED "\e[31m"
+# define ANSI_GRE "\e[32m"
+# define ANSI_YEL "\e[33m"
+# define ANSI_BLU "\e[34m"
+# define ANSI_MAG "\e[35m"
+# define ANSI_CYA "\e[36m"
 
 int	parse_perror(char *msg, int _ret);
 
--- src/parse/parse_perror.c
+++ src/parse/parse_perror.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:22:57 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/06 22:12:35 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/06 22:27:44 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	parse_perror(char *msg, int _ret)
 {
-	printf("\e[1;34mminiRT: " ANSI_RED "%s\n" ANSI_RES, msg);
+	printf(ANSI_BOL ANSI_BLU "miniRT: " ANSI_RES);
+	printf(ANSI_RED "%s\n" ANSI_RES, msg);
 	return (_ret);
 }
