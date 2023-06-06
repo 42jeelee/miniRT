94186920b8fd5cdb098025de27b2c0d9a0cd3ac4
Jeehoon Lee <jeelee@c10r2s4.42seoul.kr>
2023-06-06 22:14:09 +0900

    text miniRT is bold

--- src/parse/parse_perror.c
+++ src/parse/parse_perror.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:22:57 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/06 21:28:45 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/06 22:12:35 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 int	parse_perror(char *msg, int _ret)
 {
-	printf(ANSI_BLU "miniRT: " ANSI_RED "%s\n" ANSI_RES, msg);
+	printf("\e[1;34mminiRT: " ANSI_RED "%s\n" ANSI_RES, msg);
 	return (_ret);
 }
