5637a0f9dcac6ae5cf17218026aca1995a510091
Jeehoon Lee <jeelee@c6r9s7.42seoul.kr>
2023-06-07 13:44:30 +0900

    coding .. parsing v1

--- src/include/parse.h
+++ src/include/parse.h
@@ -6,12 +6,14 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:14:41 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/07 01:50:18 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/07 13:33:29 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
+
+# define BUFFER_SIZE 100
 
 # define ANSI_RES "\e[0m"
 # define ANSI_BOL "\e[1m"
@@ -24,9 +26,17 @@
 # define ANSI_MAG "\e[35m"
 # define ANSI_CYA "\e[36m"
 
+typedef struct s_buffer
+{
+	char	buffer[BUFFER_SIZE];
+	ssize_t	rd_size;
+	size_t	idx;
+}	t_buffer;
+
 int		vaild_file(int ac, char **av);
 
 int		parse_atoi(char *str, size_t size);
+char	*parse_gnl(int fd, t_buffer *bf);
 
 int		parse_error(char *msg, int _ret);
 void	parse_perror_exit(int _exitcode);
--- src/parse/parse_file.c
+++ src/parse/parse_file.c
@@ -6,11 +6,33 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 22:58:14 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/07 01:58:04 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/07 13:42:30 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
+
+// static void	parse_line(char *line, t_data *data)
+// {
+// 	// line에 대한 파싱작업.
+// }
+
+static void	_parsing(int fd, t_data *data)
+{
+	char		*line;
+	t_buffer	bf;
+
+	ft_memset(&bf, 0, sizeof(t_buffer));
+	while (1)
+	{
+		line = parse_gnl(fd, &bf);
+		if (!line)
+			break ;
+		// parse_line(line, data);
+		(void)data; // 지워야함
+		free(line);
+	}
+}
 
 int	parse_file(int ac, char **av, t_data *data)
 {
@@ -22,6 +44,7 @@ int	parse_file(int ac, char **av, t_data *data)
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 		parse_perror_exit(1);
+	_parsing(fd, data);
 	close(fd);
 	return (0);
 }
