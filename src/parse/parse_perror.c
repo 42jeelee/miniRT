98503c675b67c33b75d3695f3be6ad5ed0672d28
JEEHOON LEE <hoon@JEEHOONui-MacBookPro.local>
2023-06-11 02:16:01 +0900

    red text only error msg

--- src/parse/parse_perror.c
+++ src/parse/parse_perror.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:22:57 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/07 01:44:46 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/06/11 02:15:04 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ void	parse_perror_exit(int _exitcode)
 void	parse_error_exit(char *msg, int _exitcode)
 {
 	printf(ANSI_BOL ANSI_BLU "miniRT: " ANSI_RES);
-	printf(ANSI_RED "%s\n" ANSI_RES, msg);
+	printf(ANSI_RED "%s" ANSI_RES "\n", msg);
 	exit(_exitcode);
 }
 
 int	parse_error(char *msg, int _ret)
 {
 	printf(ANSI_BOL ANSI_BLU "miniRT: " ANSI_RES);
-	printf(ANSI_RED "%s\n" ANSI_RES, msg);
+	printf(ANSI_RED "%s" ANSI_RES "\n", msg);
 	return (_ret);
 }
