4df181c0dd69e8191ad0b2e69eb996241eb8a38d
황제 <ahkiler@hwangje-ui-MacBookPro.local>
2023-06-12 19:24:37 +0900

    FIX:ray

--- src/include/scene.h
+++ src/include/scene.h
@@ -6,7 +6,7 @@
 /*   By: jhwang2 <jhwang2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 14:19:46 by jhwang2           #+#    #+#             */
-/*   Updated: 2023/06/12 17:24:38 by jhwang2          ###   ########.fr       */
+/*   Updated: 2023/06/12 19:24:21 by ahkiler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,10 @@ typedef struct s_camera
 {
 	t_ray	ray;
 	t_point	n_vector; //카메라가 보는 방향
-	t_point	view_point_center;
+	t_point	center;
 	t_point	view_point_lb; //left bottom
+	t_point	horizontal;//수평길이
+	t_point	vertical;//수직길이
 	double	view_point_w; //fov 증가시 같이 증가
 	double	view_point_h; //fov 증가시 같이 증가
 	double	ratio; //종횡비, mlx의 width / height; view point의 한 길이를 설정했을때 다른 부분을 비율로 정함
@@ -35,8 +37,7 @@ typedef struct s_camera
 }	t_camera;
 
 void		init_origin_point(t_ray *ray, t_point center);
-void		init_dir(t_ray * ray, t_point orientation);
-t_point		ray_destination(t_ray *ray, double t); //원점에서 뻗어나가는 ray 직선 방정식
+void		init_dir(t_camera *cam, double u, double v);
 void		init_cam(t_data *data, t_ray ray, double ratio);
 
 #endif
--- src/scene/scene.c
+++ src/scene/scene.c
@@ -6,7 +6,7 @@
 /*   By: jhwang2 <jhwang2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 15:14:24 by jhwang2           #+#    #+#             */
-/*   Updated: 2023/06/12 17:25:59 by jhwang2          ###   ########.fr       */
+/*   Updated: 2023/06/12 19:23:36 by ahkiler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@ void	init_origin_point(t_ray *ray, t_point center)
 	ray->origin_point = set_vec (center.x, center.y, center.z);
 }
 
-void	init_dir(t_ray * ray, t_point orientation)
+void	init_dir(t_camera *cam, double u, double v) //이중 whiile문으로 u와 v값을 넣어서 ray의 방향을 바꿈
 {
-	ray->dir = set_vec (orientation.x, orientation.y, orientation.z);
-}
-
-t_point	ray_destination(t_ray *ray, double t)
-{
-	return (v_add_vec(ray->origin_point, v_mul_val (ray->dir, t)));
+	cam->ray.dir = v_unit (v_sub_vec (v_add_vec (v_add_vec (cam->view_point_lb,
+	v_mul_val (cam->horizontal, u)),
+	v_mul_val (cam->vertical, v)), cam->ray.origin_point));
 }
 
 void	init_cam(t_data *data, t_ray ray, double ratio)
 {
-	init_origin_point (&data->camera.ray, data->camera.view_point_center);
-	init_dir (&data->camera.ray, data->camera.n_vector);
+	t_point	focal;
+
+	init_origin_point (&data->camera.ray, data->camera.center);
 	data->camera.focal_length = 1.0;
 	data->camera.ratio = ratio;
 	data->camera.view_point_w = 4.0;
 	data->camera.view_point_h = data->camera.view_point_w / data->camera.ratio;
-	data->camera.view_point_lb = set_vec (data->camera.view_point_center.x - (data->camera.view_point_w / 2),
-	data->camera.view_point_center.y - (data->camera.view_point_h / 2),
-	data->camera.view_point_center.z - data->camera.focal_length);
+	focal = set_vec (0, 0, data->camera.focal_length);
+	data->camera.horizontal = set_vec (data->camera.view_point_h, 0, 0);
+	data->camera.vertical = set_vec (0, data->camera.view_point_h, 0);
+	data->camera.view_point_lb = v_sub_vec (v_sub_vec (v_sub_vec (data->camera.ray.origin_point, focal)
+		, v_div_val (data->camera.horizontal, 2)), v_div_val (data->camera.vertical, 2));
 }
