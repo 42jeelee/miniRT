9b7d507ffd3a2140ea96e7555bef9e638d3f9eed
황제 <ahkiler@hwangje-ui-MacBookPro.local>
2023-06-15 12:49:47 +0900

    FEAT: hit_plane & hit_sphere function

--- src/v_utils/dot_and_cross.c
+++ src/v_utils/dot_and_cross.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dot_and_cross.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahkiler <ahkiler@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jhwang2 <jhwang2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:42:50 by ahkiler           #+#    #+#             */
-/*   Updated: 2023/06/09 14:06:59 by ahkiler          ###   ########.fr       */
+/*   Updated: 2023/06/15 11:33:20 by jhwang2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 double	v_dot(t_point vec_origin, t_point vec_value)
 {
-	return ((vec_origin.x * vec_value.x) + (vec_origin.y * vec_value.y) + (vec_origin.z * vec_value.z));
+	return ((vec_origin.x * vec_value.x)
+		+ (vec_origin.y * vec_value.y)
+		+ (vec_origin.z * vec_value.z));
 }
 
 t_point	v_cross(t_point vec_origin, t_point vec_value)
