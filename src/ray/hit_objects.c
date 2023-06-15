d3e619579215dcaecf95ccce8f450fad6e75070c
Jeehoon Lee <jeelee@c10r1s2.42seoul.kr>
2023-06-15 18:20:21 +0900

    add create_trgb & hit_objects ... wrong operations

--- src/ray/hit_objects.c
+++ src/ray/hit_objects.c
@@ -3,19 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   hit_objects.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhwang2 <jhwang2@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:44:45 by jeelee            #+#    #+#             */
-/*   Updated: 2023/06/15 12:48:20 by jhwang2          ###   ########.fr       */
+/*   Updated: 2023/06/15 18:06:45 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
 
-double	hit_sphere(t_ray *ray, t_object *obj)
+int	r_formula(double a, double b, double c, double value[])
+{
+	double	determinant;
+
+	determinant = b * b - 4 * a * c;
+	if (determinant < 0)
+		return (0);
+	else if (determinant == 0)
+	{
+		value[0] = -1.0 * b / (2 * a);
+		return (1);
+	}
+	value[0] = (-1.0 * b + sqrt (determinant)) / (2 * a);
+	value[1] = (-1.0 * b - sqrt (determinant)) / (2 * a);
+	return (2);
+}
+
+int	hit_sphere(t_ray *ray, t_object *obj, double value[])
 {
 	t_point	o_sub_c;
-	double	determinant;
 	double	a;
 	double	b;
 	double	c;
@@ -24,53 +40,48 @@ double	hit_sphere(t_ray *ray, t_object *obj)
 	a = v_dot (ray->dir, ray->dir);
 	b = -2.0 * v_dot (o_sub_c, ray->dir);
 	c = v_dot (o_sub_c, o_sub_c) - pow (obj->diameter / 2, 2);
-	determinant = b * b - 4 * a * c;
-	if (determinant < 0 || (determinant == 0 && -1.0 * b / (2 * a) < 1.0)
-		|| (determinant > 0 && (-1.0 * b - sqrt (determinant))/ (2 * a) < 1.0
-			&& (-1.0 * b + sqrt (determinant))/ (2 * a) < 1.0))
-		return (-1.0);
-	else if (determinant == 0)
-		return (-1.0 * b / (2 * a));
-	else
-	{
-		if ((-1.0 * b - sqrt (determinant))/ (2 * a) < 1.0)
-			return ((-1.0 * b + sqrt (determinant))/ (2 * a));
-		else
-			return ((-1.0 * b - sqrt (determinant))/ (2 * a));
-	}
+	return (r_formula(a, b, c, value));
 }
 
-double	hit_plane(t_ray *ray, t_object *obj)
+int	hit_plane(t_ray *ray, t_object *obj, double value[])
 {
 	double	numer;
 	double	denom;
 
 	numer = v_dot (v_sub_vec (obj->point, ray->origin_point), obj->n_vector);
 	denom = v_dot (ray->dir, obj->n_vector);
-	if (denom == 0 || (numer / denom) < 1.0)
-		return (-1.0);
+	if (denom == 0)
+		return (0);
 	else if (numer == 0)
-		return (1.0);
+		value[0] = 0;
 	else
-		return (numer / denom);
+		value[0] = numer / denom;
+	return (1);
 }
 
-double	hit_cylinder(t_ray *ray, t_object *obj)
+int	hit_cylinder(t_ray *ray, t_object *obj, double value[])
 {
-	double	t;
+	t_point	o_sub_c;
+	double	a;
+	double	b;
+	double	c;
 
-	t = 0.0;
-	(void)ray;
-	(void)obj;
-	return (t);
+	o_sub_c = v_sub_vec(ray->origin_point, obj->point);
+	a = v_dot(ray->dir, ray->dir) - pow(v_dot(ray->dir, obj->n_vector), 2);
+	b = 2 * (v_dot(ray->dir, o_sub_c) - \
+		(v_dot(ray->dir, obj->n_vector) * v_dot(o_sub_c, obj->n_vector)));
+	c = v_dot(o_sub_c, o_sub_c) - \
+		pow(v_dot(o_sub_c, obj->n_vector), 2) - pow(obj->diameter / 2, 2);
+	return (r_formula(a, b, c, value));
 }
 
-double	hit_con(t_ray *ray, t_object *obj)
+int	hit_con(t_ray *ray, t_object *obj, double value[])
 {
-	double	t;
+	int	t;
 
-	t = 0.0;
+	t = 0;
 	(void)ray;
 	(void)obj;
+	(void)value;
 	return (t);
 }
